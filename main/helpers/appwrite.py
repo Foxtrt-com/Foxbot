5f189fedc1fc0473194aff9aa243db4b082fc16d
Chelsea Case-Miller <74470736+Chelsea-Fox@users.noreply.github.com>
2022-12-09 11:44:42 +0000

    Fixed Database connection to appwrite

--- main/helpers/appwrite.py
+++ main/helpers/appwrite.py
@@ -1,26 +1,32 @@
 import datetime
 from appwrite.client import Client
+from appwrite.exception import AppwriteException
 from appwrite.query import Query
 from appwrite.services.databases import Databases
 from . import level_helper
 
-client = Client()
 
-with open('./data/restricted/appwrite.txt') as f:
-    conf = f.readlines()
+def get_database_conn():
+    try:
+        with open('./data/restricted/appwrite.txt') as f:
+            conf = f.read().splitlines()
 
-try:
-    (client
-     .set_endpoint(conf[0])  # Your API Endpoint
-     .set_project(conf[1])  # Your project ID
-     .set_key(conf[2])  # Your secret API key
-     )
-    databases = Databases(client)
-except Exception as e:
-    print(e)
+            client = Client()
+            (client
+             .set_endpoint(conf[0])  # Your API Endpoint
+             .set_project(conf[1])  # Your project ID
+             .set_key(conf[2])  # Your secret API key
+             )
+
+            databases = Databases(client)
+    except AppwriteException as e:
+        print(e)
+
+    return databases
 
 
 def create_guild_database(guild_id):
+    databases = get_database_conn()
     databases.create(
         database_id=f"{guild_id}",
         name=f"{guild_id}",
@@ -42,12 +48,14 @@ def create_guild_database(guild_id):
 
 
 def delete_guild_database(guild_id):
+    databases = get_database_conn()
     databases.delete(
         database_id=f"{guild_id}"
     )
 
 
 def add_member(guild_id, uid, join_date):
+    databases = get_database_conn()
     databases.create_document(database_id=f"{guild_id}", collection_id='guild_users', document_id=f"{uid}",
                               data={'last_exp_drop': f"{join_date}",
                                     'lvl': 0,
@@ -55,10 +63,12 @@ def add_member(guild_id, uid, join_date):
 
 
 def delete_member(guild_id, uid):
+    databases = get_database_conn()
     databases.delete_document(database_id=f"{guild_id}", collection_id='guild_users', document_id=f"{uid}")
 
 
 def add_exp(guild_id, uid, msg_datetime, exp):
+    databases = get_database_conn()
     user = databases.get_document(database_id=f"{guild_id}", collection_id='guild_users', document_id=f"{uid}")
     if datetime.datetime.fromisoformat(user['last_exp_drop']) <= msg_datetime - datetime.timedelta(minutes=1):
         new_exp = user['exp'] + exp
@@ -84,6 +94,7 @@ def add_exp(guild_id, uid, msg_datetime, exp):
 
 
 def set_welcome_msg(guild_id, msg):
+    databases = get_database_conn()
     databases.update_document(database_id=f"{guild_id}", collection_id='guild_settings', document_id='settings',
                               data={
                                   'welcome_msg': msg
@@ -91,6 +102,7 @@ def set_welcome_msg(guild_id, msg):
 
 
 def get_welcome_msg(guild_id):
+    databases = get_database_conn()
     welcome_msg = databases.get_document(database_id=f"{guild_id}", collection_id='guild_settings',
                                          document_id='settings')
 
@@ -98,11 +110,13 @@ def get_welcome_msg(guild_id):
 
 
 def get_top_5(guild_id):
+    databases = get_database_conn()
     top_5 = databases.list_documents(database_id=f"{guild_id}", collection_id='guild_users',
                                      queries=[Query.orderDesc('exp'), Query.limit(5)])['documents']
     return top_5
 
 
 def get_user_lvl(guild_id, uid):
+    databases = get_database_conn()
     user = databases.get_document(database_id=f"{guild_id}", collection_id='guild_users', document_id=f"{uid}")
     return user
